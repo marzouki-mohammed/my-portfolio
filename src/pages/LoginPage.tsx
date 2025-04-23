414210ac735767c3a2d0c4340f3b4f57643f0f4f
marzouki <marzouki.mhammed@gmail.com>
2025-04-23 20:27:30 +0100

    add ui for chatbot

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,18 @@
-import Logo from "../components/Logo";
 
-function LoginPage() {
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { FcGoogle } from "react-icons/fc";
+
+const LoginPage = () => {
   return (
+
+    
     <div className="login-container d-flex justify-content-center align-items-center">
-         aaa
+        aaa
     </div>
+
+
+    
   );
-}
+};
 
 export default LoginPage;
